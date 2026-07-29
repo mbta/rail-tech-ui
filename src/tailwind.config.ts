55af4a4b66f6df3a8781a9db161b42221bd3c8b8
Joe Cho <jcho1@mbta.com>
2026-07-29 12:13:43 -0400

    extend specific colors

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -7,7 +7,6 @@ const {
   spacing,
   fontFamily,
   // do not export tailwindColors so as not to clash with native Tailwind colors
-  ...semanticAndSystemColors
 } = figmaTokens;
 
 const railTechUITheme = {
@@ -18,7 +17,51 @@ const railTechUITheme = {
       fontFamily,
       colors: {
         ...customColors,
-        ...semanticAndSystemColors,
+        "light-rail": {
+          "b-branch": figmaTokens.LightRail.B["b-branch"],
+          "c-branch": figmaTokens.LightRail.C["c-branch"],
+          "d-branch": figmaTokens.LightRail.D["d-branch"],
+          "e-branch": figmaTokens.LightRail.E["e-branch"],
+          "mattapan-branch": figmaTokens.LightRail.Mattapan["mattapan-branch"],
+        },
+        "heavy-rail": {
+          braintree: figmaTokens.Orbit.braintree,
+          ashmont: figmaTokens.Orbit.ashmont,
+          "orange-line": figmaTokens.Orbit.orangeline,
+          "blue-line": figmaTokens.Orbit.blueline,
+        },
+        "text-primary-light": figmaTokens.textPrimaryLight,
+        "text-primary-dark": figmaTokens.textPrimaryDark,
+        "text-secondary-light": figmaTokens.textSecondaryLight,
+        "text-secondary-dark": figmaTokens.textSecondaryDark,
+        "text-accent-light": figmaTokens.textAccentLight,
+        "text-accent-dark": figmaTokens.textAccentDark,
+        "error-state": {
+          "warning-dark": figmaTokens.errorState["warning-dark"],
+          "error-dark": figmaTokens.errorState["error-dark"],
+          "warning-bg-dark": figmaTokens.errorState["warning-bg-dark"],
+          "warning-light": figmaTokens.errorState["warning-light"],
+          "error-light": figmaTokens.errorState["error-light"],
+          "warning-bg-light": figmaTokens.errorState["warning-bg-light"],
+        },
+        "non-rev-light": figmaTokens.nonRevLight,
+        "non-rev-dark": figmaTokens.nonRevDark,
+        environment: {
+          "dev-dark": figmaTokens.environment["env-dev-dark"],
+          "dev-green-dark": figmaTokens.environment["env-dev-green-dark"],
+          "dev-blue-dark": figmaTokens.environment["env-dev-blue-dark"],
+          "dev-light": figmaTokens.environment["env-dev-light"],
+          "dev-green-light": figmaTokens.environment["env-dev-green-light"],
+          "dev-blue-light": figmaTokens.environment["env-dev-blue-light"],
+        },
+        "success-light": figmaTokens.successLight,
+        "success-dark": figmaTokens.successDark,
+        "arrivals-light": figmaTokens.arrivalsLight,
+        "arrivals-dark": figmaTokens.arrivalsDark,
+        "departures-light": figmaTokens.departuresLight,
+        "departures-dark": figmaTokens.departuresDark,
+        "icon-light": figmaTokens.iconLight,
+        "icon-dark": figmaTokens.iconDark,
       },
     },
   },
